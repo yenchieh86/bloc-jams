e7e2083dfead0f4c7352ba459af5c17affcf1909
Yen <yenchieh86@hotmail.com>
2016-12-24 08:46:10 +0000

    JavaScript-events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,27 @@
-var animatePoints = function() {
-    
-    var points = document.getElementsByClassName('point');
-    
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    for ( var x = 0; x < points.length; x ++){
-        revealPoint(x)
+var pointsArray  = document.getElementsByClassName('point');
+
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(points) {
+    forEach(points, revealPoint)
+};
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
     }
     
-};
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);   
+        }
+    });
+}
